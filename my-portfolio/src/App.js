79bff402882bc2960efb984b189ffc475e28b37d
Ashwani Pandey <ashwanipandey679@gmail.com>
2023-09-18 22:43:34 +0530

    skill and project section added

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,19 @@
 
 import './App.css';
-import { NavBar } from './components/NavBar';
+import { NavBar } from './components/NavBar.js';
+import { Banner } from './components/Banner.js';
+import { Skills } from './components/Skills';
+import { Projects } from './components/project';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Banner />
+      <Skills />
+      <Projects />
+      
     
     </div>
 
